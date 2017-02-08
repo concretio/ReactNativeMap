85c493e574a17bc8dbdbf527fa094a3fbb3ae7c9
Santosh Anand <santoshanand.jogi@gmail.com>
2017-02-08 10:06:43 +0530

    Added a output file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ReactNativeMap
 Sample application for adding map using react native
+
+**Output**
+![](./output/gmap.gif)
---
+++ app/components/Map.js
@@ -0,0 +1,64 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    };
+  }
+  render() {
+    return (
+      <MapView style={styles.container}
+        initialRegion={this.state.region}>
+        <MapView.Marker
+              title="Sample Title"
+              description="Sample description"
+              coordinate={this.state.region}
+            />
+        </MapView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
